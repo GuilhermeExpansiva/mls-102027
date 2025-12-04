8754cae9b92ccc766b12598bf4da64d1ae299a00
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-04 09:05:53 -0300

    adjust files

--- l2/decorators.ts
+++ l2/decorators.ts
@@ -5,12 +5,12 @@
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-export * from '/_100000_/customElement.js';
-export * from '/_100000_/property.js';
-export * from '/_100000_/state.js';
-export * from '/_100000_/eventOptions.js';
-export * from '/_100000_/query.js';
-export * from '/_100000_/queryAll.js';
-export * from '/_100000_/queryAsync.js';
-export * from '/_100000_/queryAssignedElements.js';
-export * from '/_100000_/queryAssignedNodes.js';
+export * from '/_102027_/customElement.js';
+export * from '/_102027_/property.js';
+export * from '/_102027_/state.js';
+export * from '/_102027_/eventOptions.js';
+export * from '/_102027_/query.js';
+export * from '/_102027_/queryAll.js';
+export * from '/_102027_/queryAsync.js';
+export * from '/_102027_/queryAssignedElements.js';
+export * from '/_102027_/queryAssignedNodes.js';
