f5bb41a15caca924d76dca5ceea70c1c3eeb0bb0
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 10:56:07 -0300

    save: l2/queryAssignedElements.defs.ts

--- l2/queryAssignedElements.defs.ts
+++ l2/queryAssignedElements.defs.ts
@@ -1,2 +1,2 @@
-/// <mls shortName="queryAssignedElements" project="102027" enhancement="_blank" folder="" />
+/// <mls fileReference="_102027_/l2/queryAssignedElements.defs.ts" enhancement="_blank" />
 
